7e65ddf89eba9667cbe8b58b8a0ff5e91dd5d4a9
LBruner <leandrobruner15@gmail.com>
2023-05-21 11:57:54 -0300

    update env key logic

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import * as http from "http";
 import app from "./app.js";
 import path from "path";
 
-const envPath = path.join(path.dirname(new URL(import.meta.url).pathname), '..', '.env');
+const envPath = path.join(__filename, '..', '..', '.env');
 dotenv.config({path: envPath});
 
 const PORT = 8080;
